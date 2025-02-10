d14091b2efc38c273bff6e6929b1bd7735f55154
Bartosz <bartoszchmielecki2003@gmail.com>
2025-02-10 21:54:30 +0100

    !

--- src/sub-pages/Kontakt.tsx
+++ src/sub-pages/Kontakt.tsx
@@ -5,7 +5,8 @@ export default function Kontakt({ darkMode }: DarkMode) {
   return (
     <>
       <h2 className="font-bold text-xl md:text-2xl text-center md:center mt-14">
-        Zostaw nam wiadmość, odezwiemy się do 24h!
+        {/* Zostaw nam wiadmość, odezwiemy się do 24h! */}
+        Test!
       </h2>
       <div className="sm:max-w-6xl mx-auto p-4 mt-4 mb-4 max-w-[98vw] min-h-[60vh]">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8 max-w-[98vw]">
